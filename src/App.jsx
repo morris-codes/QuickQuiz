e1b5d994846d4ec9c8d1b1584429ad4e11ac8527
DevOises <oiseabulu@gmail.com>
2025-02-04 21:28:16 +0100

    Split Quiz.js into multiple pages by creating separate components for each page and adding them to App.jsx Fixed the issue that caused the page to scatter when zoomed in Created a separate CSS file for each component Created a styles folder and moved all CSS files into it Placed all components inside the components folder Created the function for calculating the user's score Started working on mobile responsiveness Modified the title from Quiz-App to Quick-Quiz Added a favicon to the page Modified vite.config.js to automatically open the browser when running npm run dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import Quiz from './Components/Quiz/Quiz'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import Results from './Components/Results';
+import Problems from './Components/Problems';
 
 const App = () => {
   return (
-    <>
-     <Quiz/> 
-    </>
-  )
-}
+   <Router>
+      
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/problems" element={<Problems />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      
+    </Router>
+
+  );
+};
 
 export default App
---
+++ src/Components/Problems.jsx
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react';
+import { data } from '../assets/data';
+import Header from './Header';
+import '../Styles/Problems.css';
+import { Link } from 'react-router-dom';
+
+const Problems = () => {
+    let [index, setIndex] = useState(0);
+    let [selectedOptions, setSelectedOption] = useState([]);
+    let [question, setQuestion] = useState(data[index]);
+
+    //Option clicks
+    const optionClick = (option) => {
+        const retainOption = [...selectedOptions];
+            retainOption[index] = option;
+            setSelectedOption(retainOption);
+            selectedOptions = retainOption
+        };
+        
+    
+        //Next Button
+        const nextButton = () => {
+            if (index < data.length - 1) {
+                setIndex(index + 1);
+                setQuestion(data[index + 1]);
+            }
+        }
+        
+        //Previous Button
+        const previousButton = () => {
+            if (index > 0) {
+                setIndex(index - 1);
+                setQuestion(data[index - 1]);
+            }
+        }
+        const [allAnswers, setAllAnswers] = useState([]);
+    
+        useEffect(() => {
+            let temp = data.map(dat => dat.answer); 
+            setAllAnswers(temp); 
+        }, [data]); 
+        
+        useEffect(() => {
+        }, [allAnswers]); 
+    
+        
+        let score = 0
+        //Submit Button
+        const submitButton = () => {
+            for (let t=0; t<allAnswers.length; t++){
+                if(allAnswers[t] == selectedOptions[t]){
+                    score = score + 1
+                }
+            }
+            console.log(score);
+        };
+  return (
+    <>
+       <div className="parent">
+            <Header/>
+
+            {/* QUESTION SECTION */}
+            <div className="question-1">
+                <div className="que-1">
+                    <h6>{question.question}</h6>
+                </div>
+
+            {/* OPTIONS CATEGORY */}
+            <div className="solution">
+                <div className="option-1">
+                    <div onClick={() => optionClick(1)} 
+                        className={selectedOptions[index] === 1 ? 'selected' : ''}>
+                        <div className="option-items">
+                            <img src="/A.jpg" alt="" />
+                            <p>{question.option1}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="option-1">
+                    <div onClick={() => optionClick(2)} 
+                        className={selectedOptions[index] === 2 ? 'selected' : ''}>
+                        
+                        <div className="option-items">
+                            <img src="/B.jpg" alt="" />
+                            <p>{question.option2}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="option-1">
+                    <div onClick={() => optionClick(3)} 
+                        className={selectedOptions[index] === 3 ? 'selected' : ''}>
+                        <div className="option-items">
+                            <img src="/C.jpg" alt="" />
+                            <p>{question.option3}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="option-1">
+                    <div onClick={() => optionClick(4)} 
+                        className={selectedOptions[index] === 4 ? 'selected' : ''}>
+                        <div className="option-items">
+                            <img src="/D.jpg" alt="" />
+                            <p>{question.option4}</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            </div>
+            <div className="nav">
+                <button onClick={previousButton} disabled={index===0}>Previous</button>
+                {index === data.length - 1 ? (
+                  <Link className='submit-link' to='/results'><button className='submit-button' onClick={submitButton}>Submit</button></Link>       
+                ):(
+                <button onClick={nextButton}>Next</button>
+                )}
+            </div>
+                  <div className='index'><i><b>{index + 1} of {data.length}</b></i></div>
+        </div>
+    </>
+  )
+}
+
+export default Problems
